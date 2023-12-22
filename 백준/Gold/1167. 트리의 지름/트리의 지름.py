c0ed63e2df5b31e485f463b2a5752ab3d5a801fb
wdyneed <88184651+wdyneed@users.noreply.github.com>
2023-12-22 22:50:47 +0900

    [Gold II] Title: 트리의 지름, Time: 612 ms, Memory: 71356 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -7,8 +7,10 @@ graph = [[] * V for _ in range(V + 1)]
 visited_dfs = [-1] * (V + 1)
 
 def dfs(node, weight):
+    # 원하는 노드에서 다른 노드들(ex:1번 노드에서 나머지 노드들)까지의 거리 구하는 함수
     for n, l in graph[node]:
         cost = weight + l
+        # 방문을 하지 않았다면, 가중치 저장
         if visited_dfs[n] == -1:
             visited_dfs[n] = cost
             dfs(n, cost)
@@ -27,13 +29,14 @@ for i in range(1, V + 1):
 visited_dfs[1] = 0
 
 dfs(1, 0)
-
+# 노드 1에서 시작해서 가장 먼 노드 찾기(인덱스)
 idx = visited_dfs.index(max(visited_dfs))
 
+# 방문 리스트 초기화
 visited_dfs = [-1] * (V + 1)
-
 visited_dfs[idx] = 0
 
+# 임의의 노드에서 가장 먼 노드를 찾고 거기서 가장 먼 노드를 찾으면 그것이 트리의 지름
 dfs(idx, 0)
 
 print(max(visited_dfs))
