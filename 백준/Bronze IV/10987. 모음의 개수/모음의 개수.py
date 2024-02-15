f317859aad15dff4421d927dd89f2afda74320f7
wdyneed <88184651+wdyneed@users.noreply.github.com>
2024-02-15 09:26:24 +0900

    [Bronze IV] Title: 모음의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze IV/10987. 모음의 개수/모음의 개수.py
@@ -0,0 +1,10 @@
+import sys
+
+word = sys.stdin.readline().rstrip()
+count = 0
+
+for i in word:
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        count += 1
+
+print(count)
